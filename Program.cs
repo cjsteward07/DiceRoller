8833f1a5a6c81de70028e467257877f7063862ca
unknown <cjsteward07@gmail.com>
2022-10-13 14:13:00 -0400

    added dev mode for testing

--- Program.cs
+++ Program.cs
@@ -79,30 +79,117 @@ string TenSidedDice(int firstRandomRoll, int secondRandomRoll, int diceTotal)
 void Menu()
 {
     bool isValidInput = false;
-    int diceSides;
-
-    Console.WriteLine("-Dice Roller Game-");
+    int diceSides = 0;
+    int menuChoice;
+    int firstRoll;
+    int secondRoll;
+    int diceTotal = 0;
+    int firstRandomRoll = 0;
+    int secondRandomRoll = 0;
 
     do
     {
-        Console.WriteLine("Please enter the number of sides for the dice");
-        isValidInput = int.TryParse(Console.ReadLine(), out diceSides);
+        Console.WriteLine("Please enter a number from the menu below:");
+        Console.WriteLine("1.) Play Game (Random)");
+        Console.WriteLine("2.) Dev Mode (User Input)");
+        isValidInput = int.TryParse(Console.ReadLine(), out menuChoice);
         if (isValidInput == false)
         {
             Console.Clear();
-            Console.WriteLine("Invalid input. Please enter a valid number: \n");
+            Console.WriteLine("Invalid input. Please enter a valid integer from the menu: \n");
         }
     } while (isValidInput == false);
 
-    Console.Clear();
-    Console.WriteLine($"Press any key to roll the {diceSides} sided dice: ");
-    Console.ReadKey();
+    switch(menuChoice)
+    {
+        case 1:
+            Console.Clear();
+            do
+            {
+                Console.WriteLine("Please enter the number of sides for the dice");
+                isValidInput = int.TryParse(Console.ReadLine(), out diceSides);
+                if (isValidInput == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please enter a valid number: \n");
+                }
+            } while (isValidInput == false);
 
-    var randomGeneratorReturn = RandomGenerator(diceSides);
+            Console.Clear();
+            Console.WriteLine($"Press any key to roll the {diceSides} sided dice: ");
+            Console.ReadKey();
 
-    int firstRandomRoll = randomGeneratorReturn.Item1;
-    int secondRandomRoll = randomGeneratorReturn.Item2;
-    int diceTotal = randomGeneratorReturn.Item3;
+            var randomGeneratorReturn = RandomGenerator(diceSides);
+
+            firstRandomRoll = randomGeneratorReturn.Item1;
+            secondRandomRoll = randomGeneratorReturn.Item2;
+            diceTotal = randomGeneratorReturn.Item3;
+            break;
+
+        case 2:
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Developer Mode Enabled\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("SixSidedDiceCombo Method. Requires user input of '6' sides:");
+            Console.ResetColor();
+            Console.WriteLine("Both rolls = 1: 'Snake Eyes'");
+            Console.WriteLine("Both rolls = 6: 'Box Cars'");
+            Console.WriteLine("Combination of 1 and 2: 'Ace Deuce'\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("SixSidedDiceTotal Method. Requires user input of '6' sides:");
+            Console.ResetColor();
+            Console.WriteLine("Roll total = 7 or 11: 'Congrats, you've won!'");
+            Console.WriteLine("Roll total = 2, 3, or 12: Message from both 'SixSidedDiceCombo' and 'SixSidedDiceTotal' methods\n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("TenSidedDice Method. Requires user input of '10' sides:");
+            Console.ResetColor();
+            Console.WriteLine("Roll total = 20: 'Congrats, double tens. You've won!'");
+            Console.WriteLine("Roll total = 15: '15, my favorite number!'\n\n");
+            Console.ResetColor();
+
+            do 
+            {
+                Console.WriteLine("Please enter the number of sides for the dice:");
+                isValidInput = int.TryParse(Console.ReadLine(), out diceSides);
+                if (isValidInput == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please enter a valid number: \n");
+                }
+            } while (isValidInput == false);
+            do
+            {
+                Console.WriteLine("Please enter the number for the first roll:");
+                isValidInput = int.TryParse(Console.ReadLine(), out firstRoll);
+                if (isValidInput == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please enter a valid number: \n");
+                }
+            } while (isValidInput == false);
+            do
+            {
+                Console.WriteLine("Please enter the number for the second roll:");
+                isValidInput = int.TryParse(Console.ReadLine(), out secondRoll);
+                if (isValidInput == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid input. Please enter a valid number: \n");
+                }
+            } while (isValidInput == false);
+
+            firstRandomRoll = firstRoll;
+            secondRandomRoll = secondRoll;
+            diceTotal = firstRoll + secondRoll;
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Invalid input, please choose a '1' or a '2' from the menu.");
+            Menu();
+            break;
+    }
 
     Console.WriteLine($"Your first dice roll was {firstRandomRoll}");
     Console.WriteLine($"Your second dice roll was {secondRandomRoll}");
@@ -112,13 +199,17 @@ void Menu()
     {
         string sixSidesCombo = SixSidedDiceCombo(firstRandomRoll, secondRandomRoll);
         string sixSidesTotal = SixSidedDiceTotal(diceTotal);
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine(sixSidesCombo);
-        Console.WriteLine(diceTotal);
+        Console.WriteLine(sixSidesTotal);
+        Console.ResetColor();
     }
     else if(diceSides == 10)
     {
         string tenSides = TenSidedDice(firstRandomRoll, secondRandomRoll, diceTotal);
+        Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine(tenSides);
+        Console.ResetColor();
     }
 
     Console.WriteLine("Would you like to play again? [y]/[n]: ");
@@ -126,14 +217,14 @@ void Menu()
     {
         isValidInput = false;
 
-        string menuChoice = Console.ReadLine().ToLower();
+        string playAgain = Console.ReadLine().ToLower();
 
-        if (menuChoice == "y" || menuChoice == "yes")
+        if (playAgain == "y" || playAgain == "yes")
         {
             Console.Clear();
             Menu();
         }
-        else if (menuChoice == "n" || menuChoice == "no")
+        else if (playAgain == "n" || playAgain == "no")
         {
             Environment.Exit(0);
         }
